f051e1aea9aba7bc1689acae60d990bfd6d4634a
Yuki Takeichi <yuki.takeichi@gmail.com>
2016-12-24 19:27:27 +0900

    test for lock

--- tests/db.rs
+++ tests/db.rs
@@ -1,25 +1,38 @@
 extern crate reveldb;
+extern crate nix;
 
 use reveldb::*;
-
-// XXX: cargo runでassert!に失敗した際に
-// テスト全体の実行が止まらないようにする方法を探す
-
-// #[test]
-// fn posix_file_lock() {
-// let env1 = Env::new();
-// let env2 = Env::new();
-// let dbname = "testdb";
-//
-// let db1 = DB::new(&env1, dbname);
-// assert!(db1.is_ok());
-// let db2 = DB::new(&env2, dbname);
-// assert!(db2.is_err());
-// }
-//
+use nix::unistd::fork;
+use nix::unistd::ForkResult::*;
+use nix::sys::wait::waitpid;
+use std::{process, thread, time};
 
 #[test]
-fn duplicate_lock_file() {
+fn protect_from_different_process() {
+    let dbname = "testdb";
+    match fork() {
+        Ok(Parent { child }) => {
+            let env1 = Env::new();
+            let db1 = DB::new(&env1, dbname);
+            assert!(db1.is_ok());
+
+            assert!(waitpid(child, None).is_ok());
+        }
+        Ok(Child) => {
+            thread::sleep(time::Duration::new(1, 0));
+
+            let env2 = Env::new();
+            let db2 = DB::new(&env2, dbname);
+            assert!(db2.is_err());
+
+            process::exit(0);
+        }
+        Err(_) => panic!("Error: Fork Failed"),
+    }
+}
+
+#[test]
+fn protect_multiple_uses_from_the_same_process() {
     let env = Env::new();
     let dbname = "testdb";
 
